6072984ea9fdff6c18bc4ea628402a389761f50a
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2025-07-19 02:46:07 -0700

    fix broken import

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,7 +1,7 @@
 import gallery from './gallery';
 import service from './service';
 import additions from './additions'  
-import specials from './Specials';
 import availability from './availability';
+import specials from './specials';
 
 export const schemaTypes = [gallery, service, additions, specials, availability];
